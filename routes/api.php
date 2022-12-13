5585f0c8ed62cbb47bdb2e8a795c4b27ac6f0b70
Ashim Kumar Shaha <ashimkumar69@gmail.com>
2022-12-13 23:11:21 +0600

    dingo api and sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,17 +15,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix("auth")->controller(AuthController::class)
-    ->group(function () {
-        Route::post("/login", 'login')->middleware(['throttle:3,1']);
-        Route::post("/logout", 'logout')->middleware(['auth:sanctum']);
-        Route::post('/forgot-password', 'forgotPassword')->middleware(['throttle:3,1']);
-        Route::post('/reset-password', 'resetPassword');
-    });
+$api = app('Dingo\Api\Routing\Router');
 
-Route::prefix("admin")->middleware('auth:sanctum')
-    ->controller(UserController::class)->group(function () {
-        Route::get("/user", 'index');
-        Route::post("/send-verify-email", 'sendVerificationMail');
-        Route::post("/verify-email", 'verifyMail');
+$api->version('v1', function ($api) {
+
+    $api->group(["prefix" => "auth"], function ($api) {
+
+        $api->group(['middleware' => 'api.throttle', 'limit' => 100, 'expires' => 1], function ($api) {
+
+            $api->post("/login", [AuthController::class, "login"]);
+            // $api->post("/forgot-password", 'forgotPassword');
+            // $api->post("/reset-password", 'resetPassword');
+        });
+        $api->group(['middleware' => 'auth:sanctum'], function ($api) {
+            $api->post("/logout", [AuthController::class, "logout"]);
+        });
     });
+    $api->group(['middleware' => 'auth:sanctum', "prefix" => "admin"], function ($api) {
+        $api->get("/user", [UserController::class, 'index']);
+        // $api->post("/send-verify-email", 'sendVerificationMail');
+        // $api->post("/verify-email", 'verifyMail');
+    });
+});
+
+// Route::prefix("auth")->controller(AuthController::class)
+//     ->group(function () {
+//         Route::post("/login", 'login')->middleware(['throttle:3,1']);
+//         Route::post("/logout", 'logout')->middleware(['auth:sanctum']);
+//         Route::post('/forgot-password', 'forgotPassword')->middleware(['throttle:3,1']);
+//         Route::post('/reset-password', 'resetPassword');
+//     });
+
+// Route::prefix("admin")->middleware('auth:sanctum')
+//     ->controller(UserController::class)->group(function () {
+//         Route::get("/user", 'index');
+//         Route::post("/send-verify-email", 'sendVerificationMail');
+//         Route::post("/verify-email", 'verifyMail');
+//     });
